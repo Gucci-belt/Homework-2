2427cf31b28a596e3de6f40a48cfed955dcb9209
Gucci-belt <doctorjekyll2548@gmail.com>
2025-11-10 11:08:26 +0700

    test

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ Version 1: The Buggy Code
 BUG: ข้อความทักทายผิด (ใช้ "Hi" แทน "Hello")
 """
 def greet(name):
-    # นี่คือบั๊กที่ชัดเจนมาก
-    return f"Hi, {name}!" # <--- BUG!
+    
+    return f"Hello, {name}!" 
