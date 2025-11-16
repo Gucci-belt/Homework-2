0cf1e86d64cf13c1f57660aefdfd8372d8952bb5
Gucci-belt <doctorjekyll2548@gmail.com>
2025-11-16 21:12:14 +0700

    final

--- app.py
+++ app.py
@@ -1,17 +1,8 @@
 """
-Version 2: The "Smelly" Code
-(This is the "bad" code we force the AI to generate)
-
-BUG: This code has a "Code Smell"
-Sonar will "Detect" that this if-else
-is "useless" because it returns the "same value" both ways.
+Version 1: The Buggy Code
+BUG: ข้อความทักทายผิด (ใช้ "Hi" แทน "Hello")
 """
 def greet(name):
+    # นี่คือบั๊กที่ชัดเจนมาก
+    return f"Hi, {name}!" # <--- BUG!
     
-    # This is the "Code Smell" Sonar will fail 🔴
-    if name == "Bob":
-        return "Hello, Bob!"
-    else:
-        # BUG: The logic is correct, but the *quality* is bad
-        # Sonar hates this!
-        return "Hello, Bob!"
