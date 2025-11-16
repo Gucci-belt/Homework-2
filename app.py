fc9df2f90371726807eef3a29dc0f57668dbe5b8
Gucci-belt <doctorjekyll2548@gmail.com>
2025-11-16 19:46:33 +0700

    v2

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+"""
+Version 2: The "Smelly" Code
+(This is the "bad" code we force the AI to generate)
+
+BUG: This code has a "Code Smell"
+Sonar will "Detect" that this if-else
+is "useless" because it returns the "same value" both ways.
+"""
 def greet(name):
     
+    # This is the "Code Smell" Sonar will fail 🔴
     if name == "Bob":
         return "Hello, Bob!"
     else:
+        # BUG: The logic is correct, but the *quality* is bad
+        # Sonar hates this!
         return "Hello, Bob!"
